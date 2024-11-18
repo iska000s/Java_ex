ae37f9d32eae2fb684b79c46fb74065475c5548f
Izabela Stankiewicz <izabelast@op.pl>
2024-11-18 20:36:26 +0100

    Start with Java

---
+++ Java_maraton.java
@@ -0,0 +1,27 @@
+// public class Test {
+//     public static void main(String[] args){
+//         System.out.println("Hello Java");
+//         System.out.print("Hello Coders Lab");
+//     }
+// }
+
+public class Test {
+    public static void main (String[] args){
+        String name;
+        name = "Iza";
+        
+        int number = 0;
+        char d = 'B';
+        double numbers = 2.3;
+        boolean exam = false;
+        String city = "Warsaw";
+        
+        // System.out.println("Witaj, " + name + "!");
+        // System.out.printf("Witaj %s!", name);
+        
+        System.out.println("Wynik : " + number);
+        System.out.println("Wynik : " + d);
+        System.out.println("Wynik : " + numbers);
+        
+    }
+}
